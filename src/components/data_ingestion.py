bf89b8d634f3d0da40c185fc5f4a8653c39d6433
sadiya yasmeen <yasminsadiya51@gmail.com>
2025-02-27 21:10:47 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,13 +11,14 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 @dataclass # decorator ,inside a class to define a class variable we basically using init but if we use dataclass we will be able to define the data class
 
 class DataIngestionConfig:
-    train_data_path:str = os.path.join('artifacts','train.csv')
-    test_data_path:str = os.path.join('artifacts','test.csv')
-    raw_data_path:str = os.path.join('artifacts','raw.csv')
+    train_data_path:str = os.path.join('artifacts',"train.csv")
+    test_data_path:str = os.path.join('artifacts',"test.csv")
+    raw_data_path:str = os.path.join('artifacts',"raw.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -46,6 +47,8 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.inititate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
--- src/utils.py
+++ src/utils.py
@@ -4,11 +4,27 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
-def save_object(file_path,obj):
+from sklearn.metrics import r2_score
+import pickle
+def save_object(file_path, obj):
     try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj,file_obj)
+        dir_path = os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure directory exists
+        with open(file_path, 'wb') as f:
+            dill.dump(obj, f)  # Save object as pickle
     except Exception as e:
         raise CustomException(e, sys)
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        # X_train,X_test,Y_train,Y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train) # train model
+            y_train_pred = model.predict(X_train)
+            y_test_predict = model.predict(X_test)
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_predict)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except :
+        pass
